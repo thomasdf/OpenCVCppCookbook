006a4464022246c9f6342ae02046ed9329566216
Thomas Frøysa <thomasdf@stud.ntnu.no>
2016-12-29 09:31:50 +0100

    add first example-project
    an example where an image is shown in a window

---
+++ myFistOpenCVConsoleProject/main.cpp
@@ -0,0 +1,13 @@
+#include <opencv2\core\core.hpp>
+#include <opencv2\highgui\highgui.hpp>
+
+int main() {
+	//read an image
+	cv::Mat image = cv::imread("img.jpg");
+	//create image window named "My Image"
+	cv::namedWindow("My Image");
+	//show the image on the window
+	cv::imshow("My Image", image);
+	//wait key for 5000 ms
+	cv::waitKey(5000);
+}
